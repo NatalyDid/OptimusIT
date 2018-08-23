3c295d263571b5fd03957936cdf8bfe856288528
NatalyDid <nat.khom@ukr.net>
2018-08-23 09:31:10 +0300

    files updated

--- app/js/header.js
+++ app/js/header.js
@@ -24,8 +24,8 @@ $(function () {
         arrows: true,
         prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
-         slidesToShow: 3,
-         slidesToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1200,
--- app/js/portfolio.js
+++ app/js/portfolio.js
@@ -9,4 +9,83 @@ $(document).ready(function () {
             cssStyle: 'light-theme'
         });
     }
+
+    function tr(box, height) {
+        var $box = $(box);
+
+        if (typeof $box.data('text') == 'undefined') {
+            $box.data('text', box.innerHTML);
+        }
+        var text = $box.data('text');
+        var clone = document.createElement('div');
+
+        clone.style.position = 'absolute';
+        clone.style.visibility = 'hidden';
+        clone.style.top = '100%';
+        clone.style.width = box.clientWidth + 'px';
+        clone.innerHTML = text;
+        document.body.appendChild(clone);
+        console.log(box.offsetWidth);
+        var l = text.length - 1;
+
+        for (; l >= 0 && clone.offsetHeight > height; l--) {
+            clone.innerHTML = text.substring(0, l) + '...';
+        }
+
+        box.innerHTML = clone.innerHTML;
+
+        if (!('remove' in Element.prototype)) {
+            Element.prototype.remove = function () {
+                if (this.parentNode) {
+                    this.parentNode.removeChild(this);
+                }
+            };
+        }
+        clone.remove();
+    }
+
+    function refreshTr() {
+        var item_main = document.querySelectorAll('.slide-text-wrapper');
+        for (var i = 0; i < item_main.length; i++) {
+            tr(item_main[i], 80)
+        }
+    }
+
+    refreshTr();
+    var timeout;
+    $(window).resize(function () {
+        timeout && clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            refreshTr();
+            clearTimeout(timeout);
+        }, 100);
+    });
+
+    $('.portfolio-item-slider').slick({
+        dots: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    // slidesToShow: 2,
+                    // slidesToScroll: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    // slidesToShow: 1,
+                    // slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
 });
